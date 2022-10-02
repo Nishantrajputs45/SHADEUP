46c5ee395148af73d3c1da4cc8b64aab2894c796
Nshantrajputs45 <nishantrajputs45@gmail.com>
2022-10-02 15:45:19 +0530

    HEX to RGB converter function designed

--- js/main.js
+++ js/main.js
@@ -1,28 +1,30 @@
-function randompickerbtn() {
-  
-}
+function randompickerbtn() {}
 
-function hexFindbtn() {
-}
+function hexFindbtn() {}
 
-function RGBFindbtn() {
-  
-}
+function RGBFindbtn() {}
 
-function RangeSelector() {
-  
-}
+function RangeSelector() {}
 
 // hex to rgb converter
 
 String.prototype.convertToRGB = function () {
-  
+  var aRgbHex = this.match(/.{1,2}/g);
+  var aRgb = [
+    parseInt(aRgbHex[0], 16),
+    parseInt(aRgbHex[1], 16),
+    parseInt(aRgbHex[2], 16),
+  ];
+  return aRgb;
 };
 
 function hexConvbtn() {
-  
+  const hexCInp = document.querySelector("#hexCInp");
+  const hexCOut = document.querySelector("#hexCOut");
+
+  const rgb = hexCInp.value.convertToRGB();
+
+  hexCOut.innerText = rgb;
 }
 
-function rgbConvbtn() {
-  
-}
+function rgbConvbtn() {}
